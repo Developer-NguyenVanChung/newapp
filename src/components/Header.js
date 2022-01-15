2764e35546b0f5938fad87c0e409a1ec473e3e49
Developer-NguyenVanChung <chunghcs@gmail.com>
2022-01-15 16:48:35 +0700

    edit Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,26 +7,8 @@ function Header() {
 
     return (
         <div className='pageHeader1'>
-            <Nav
-                activeKey="/home"
-                onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-            >
-                <Nav.Item>
-                    <Nav.Link href="/home">Active</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="link-1">Link</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="link-2">Link</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="disabled" disabled>
-                        Disabled
-                    </Nav.Link>
-                </Nav.Item>
-            </Nav>
             <h1>This is Header of web page</h1>
+            <p>Dieu huong o day</p>
         </div>
     )
 }
